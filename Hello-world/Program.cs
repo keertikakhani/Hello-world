26cedf39214f2ccddd28eca54b08bb0f4bfbaf9b
Your Name <you@example.com>
2022-12-13 22:33:47 +0530

    Adding Hello world program

---
+++ Hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace new_project
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
